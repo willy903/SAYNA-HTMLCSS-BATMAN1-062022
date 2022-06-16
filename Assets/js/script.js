6e76bd87f5f9aba8af17270ab159045a4d3f7cbd
willy903 <willyjacquino@gmail.com>
2022-06-16 21:22:01 +0100

    petite modification

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,24 +1,3 @@
-let nxt = document.querySelector(".btn_next");
-let prv = document.querySelector(".btn_preve");
-let i = 1;
-
-nxt.addEventListener("click", () => {
-    let allSlides = Array.from(document.querySelectorAll(".slide"));
-    if (allSlides.length > 1) {
-        let currentShownIndex = allSlides.findIndex(slide => slide.classList.contains("slide-shown"))
-        allSlides[currentShownIndex].classList.remove("slide-shown")
-        allSlides[(currentShownIndex + 1) % allSlides.length].classList.add("slide-shown")
-    }
-})
-
-prv.addEventListener("click", () => {
-    let allSlides = Array.from(document.querySelectorAll(".slide"));
-    let currentShownIndex = allSlides.findIndex(slide => slide.classList.contains("slide-shown"))
-    allSlides[currentShownIndex].classList.remove("slide-shown")
-    console.log(currentShownIndex)
-    allSlides[(currentShownIndex + 1) % allSlides.length].classList.add("slide-shown")
-})
-
 let btply = document.querySelector(".play_icon");
 
 btply.addEventListener("click", () => {
